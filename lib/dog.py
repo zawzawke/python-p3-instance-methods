8a78e242558a3b43a2832ca87df834eb88f3f741
Paul <paulallanmain@gmail.com>
2025-05-14 14:41:05 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):   
+        print("The dog is sitting.")
+fido = Dog()
+fido.sit()  
 
-    #Instance method definition
-    pass
+
+
